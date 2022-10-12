9d6185754d77b04aa251889f994c5399ec5c7286
Marc-Meller <mpmc2000@hotmail.com>
2022-10-11 20:43:35 -0500

    Modificacion 3

--- app/src/main/java/com/marcmeller/agenda_online/MenuPrincipal.java
+++ app/src/main/java/com/marcmeller/agenda_online/MenuPrincipal.java
@@ -1,23 +1,37 @@
 package com.marcmeller.agenda_online;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
+
 
     Button CerrarSesion;
 
     FirebaseAuth firebaseAuth;
     FirebaseUser user;
 
+    TextView NombresPrincipal, CorreoPrincipal;
+    ProgressBar progressBarDatos;
+
+    DatabaseReference Usuarios;
 
 
     @Override
@@ -25,14 +39,70 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Agenda Online");
+
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal = findViewById(R.id.CorreoPrincipal);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
+
+        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
         CerrarSesion = findViewById(R.id.CerrarSesion);
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+
+
 
         CerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SalirApliacion();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        ComprobarInicioSesion();
+        super.onStart();
+    }
+
+    private void ComprobarInicioSesion(){
+        if(user!= null){
+            //El usuario ha iniciado sesion
+            CargaDeDatos();
+        }else {
+            //Lo dirigira al MainActivity
+            startActivity(new Intent(MenuPrincipal.this,MainActivity.class));
+        }
+    }
+
+    private void CargaDeDatos(){
+        Usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //Si el usuario existe
+                if(snapshot.exists()){
+                    progressBarDatos.setVisibility(View.GONE);
+
+                    //Los TextView se muestran
+                    NombresPrincipal.setVisibility(View.VISIBLE);
+                    CorreoPrincipal.setVisibility(View.VISIBLE);
+
+                    //Obtener los datos
+                    String nombres = "" +snapshot.child("nombre").getValue();
+                    String correo = ""+snapshot.child("correo").getValue();
+
+                    //Set datos en los respectios TextView
+                    NombresPrincipal.setText(nombres);
+                    CorreoPrincipal.setText(correo);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
